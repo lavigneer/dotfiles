e72cd2bd1432c5b7fbe94567ff770315f366e164
Eric Lavigne <elavigne@evertz.com>
2022-12-16 13:19:28 -0500

    Fix issue with colorscheme not applying properly to new windows

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,5 +1,4 @@
-local	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
-
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
+require('rose-pine').setup({
+	disable_background = true,
+})
+vim.cmd.colorscheme("rose-pine")
