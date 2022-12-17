f7074e0eb2eb1a12332f65516298f3771850915d
Eric Lavigne <elavigne@evertz.com>
2022-12-16 23:02:49 -0500

    Adjust telescope layouts and add diagnostics menu

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -2,3 +2,23 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find files" })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Live grep" })
 vim.keymap.set('n', '<leader>fo', builtin.oldfiles, { desc = "Old files" })
+vim.keymap.set('n', '<leader>ld', builtin.diagnostics, { desc = "Diagnostics" })
+
+require('telescope').setup({
+    defaults = {
+        sorting_strategy = "ascending",
+        layout_config = {
+            horizontal = {
+                prompt_position = "top",
+                preview_width = 0.55,
+                results_width = 0.8,
+            },
+            vertical = {
+                mirror = false,
+            },
+            width = 0.87,
+            height = 0.80,
+            preview_cutoff = 120,
+        }
+    }
+})
