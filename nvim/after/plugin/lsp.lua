bbc72718126270f40ee64a4827c5222f7236db84
Eric Lavigne <elavigne@evertz.com>
2023-02-07 09:23:12 -0500

    Add cspell and search for word under cursor

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -63,6 +63,8 @@ null_ls.setup({
     sources = {
         null_ls.builtins.formatting.prettierd,
         null_ls.builtins.diagnostics.eslint_d,
+        null_ls.builtins.diagnostics.cspell,
+        null_ls.builtins.code_actions.cspell,
         require("typescript.extensions.null-ls.code-actions"),
     }
 })
--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,6 +1,7 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find files" })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Live grep" })
+vim.keymap.set('n', '<leader>fG', builtin.grep_string, { desc = "Grep string under cursor" })
 vim.keymap.set('n', '<leader>fo', builtin.oldfiles, { desc = "Old files" })
 vim.keymap.set('n', '<leader>ld', builtin.diagnostics, { desc = "Diagnostics" })
 vim.keymap.set('n', '<leader>fs', builtin.git_status, { desc = "Git Status" })
