6621750198add11f5a4839f2f2616b0a21d1b079
Eric Lavigne <elavigne@evertz.com>
2023-04-10 10:18:11 -0400

    Reverse floating terminal keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,6 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local Util = require("lazyvim.util")
+
 -- Centering movements
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
@@ -18,3 +20,7 @@ vim.keymap.set("v", "<leader>d", "\"_d")
 
 -- Force out of insert mode via ctrl+c
 vim.keymap.set("i", "<C-c>", "<Esc>")
+
+vim.keymap.set("n", "<leader>fT", function() Util.float_term(nil, { cwd = Util.get_root() }) end,
+{ desc = "Terminal (root dir)" })
+vim.keymap.set("n", "<leader>ft", function() Util.float_term() end, { desc = "Terminal (cwd)" })
