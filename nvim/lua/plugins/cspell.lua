c3f0312529484478194edf333d5e3e2a501b8274
Eric Lavigne <hi_eric@hotmail.com>
2023-04-20 20:18:57 -0400

    Don't always ensure cspell is installed as it can get in the way sometimes

--- nvim/lua/plugins/cspell.lua
+++ nvim/lua/plugins/cspell.lua
@@ -1,16 +1,10 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        opts = function(_, opts)
-            table.insert(opts.ensure_installed, "cspell")
-        end,
-    },
-    {
-        "jose-elias-alvarez/null-ls.nvim",
-        opts = function(_, opts)
-            local nls = require("null-ls")
-            table.insert(opts.sources, nls.builtins.diagnostics.cspell)
-            table.insert(opts.sources, nls.builtins.code_actions.cspell)
-        end,
-    },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      table.insert(opts.sources, nls.builtins.diagnostics.cspell)
+      table.insert(opts.sources, nls.builtins.code_actions.cspell)
+    end,
+  },
 }
