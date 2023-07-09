7e53e2940646e9ebe772a451325513e585488a46
Eric Lavigne <hi_eric@hotmail.com>
2023-07-09 13:44:09 -0400

    Add neotest-rust

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -2,6 +2,6 @@ return {
   { "nvim-neotest/neotest-jest" },
   {
     "nvim-neotest/neotest",
-    opts = { adapters = { "neotest-jest" } },
+    opts = { adapters = { "neotest-jest", "neotest-rust" } },
   },
 }
