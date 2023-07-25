39f348cdaaae350a4d2b1dc74c746ab3172676bc
Eric Lavigne <hi_eric@hotmail.com>
2023-07-24 22:02:25 -0400

    Add treesitter commentstring

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -39,6 +39,15 @@ return {
 	{
 		"echasnovski/mini.comment",
 		config = true,
+		event = "VeryLazy",
+		opts = {
+			options = {
+				custom_commentstring = function()
+					return require("ts_context_commentstring.internal").calculate_commentstring()
+						or vim.bo.commentstring
+				end,
+			},
+		},
 	},
 	{
 		"echasnovski/mini.completion",
