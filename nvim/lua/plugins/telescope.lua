d3671b87cc98c0582a31271df9390db90af57f55
Eric Lavigne <hi_eric@hotmail.com>
2023-07-24 21:04:54 -0400

    Tweak telescope layout

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -35,6 +35,13 @@ return {
 						end,
 					},
 				},
+				layout_config = {
+					horizontal = { prompt_position = "top", preview_width = 0.55 },
+					vertical = { mirror = false },
+					width = 0.87,
+					height = 0.80,
+					preview_cutoff = 120,
+				},
 			},
 			extensions = {
 				["ui-select"] = {
