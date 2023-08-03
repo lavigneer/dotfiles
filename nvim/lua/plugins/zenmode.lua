07778b88808a5c2ef21dcf6fb47bd53403bd51ca
Eric Lavigne <elavigne@evertz.com>
2023-08-03 13:11:01 -0400

    Add shortcut for toggling zen mode

--- nvim/lua/plugins/zenmode.lua
+++ nvim/lua/plugins/zenmode.lua
@@ -1,3 +1,12 @@
 return {
   "folke/zen-mode.nvim",
+  keys = {
+    {
+      "<leader>uz",
+      function()
+        require("zen-mode").toggle()
+      end,
+      desc = "Toggle zen mode",
+    },
+  },
 }
