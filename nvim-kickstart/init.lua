a0a491f890d8c1aaceb69c0fd6d017e7cd102266
Eric Lavigne <hi_eric@hotmail.com>
2023-11-08 18:17:19 -0500

    Disable shift up/down in insert mode of nvim to avoid accidental paging

--- nvim-kickstart/init.lua
+++ nvim-kickstart/init.lua
@@ -464,6 +464,8 @@ vim.o.foldenable = false
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+vim.keymap.set( "i", "<S-Up>", "<Nop>", { silent = true })
+vim.keymap.set("i", "<S-Down>", "<Nop>", { silent = true })
 
 -- Remap for dealing with word wrap
 vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
