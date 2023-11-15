72777d79755840ba5301e2a32254e9254fe7019e
Eric Lavigne <hi_eric@hotmail.com>
2023-11-15 08:28:23 -0500

    Remove unused plugins and replace comment with mini.comment

--- nvim-kickstart/init.lua
+++ nvim-kickstart/init.lua
@@ -186,6 +186,10 @@ require("lazy").setup({
     opts = {},
   },
   {
+    "echasnovski/mini.comment",
+    opts = {},
+  },
+  {
     "echasnovski/mini.bufremove",
     opts = {},
     keys = {
@@ -252,49 +256,6 @@ require("lazy").setup({
 
   -- Useful plugin to show you pending keybinds.
   { "folke/which-key.nvim", opts = {} },
-  {
-    -- Adds git related signs to the gutter, as well as utilities for managing changes
-    "lewis6991/gitsigns.nvim",
-    opts = {
-      -- See `:help gitsigns.txt`
-      signs = {
-        add = { text = "+" },
-        change = { text = "~" },
-        delete = { text = "_" },
-        topdelete = { text = "‾" },
-        changedelete = { text = "~" },
-      },
-      on_attach = function(bufnr)
-        vim.keymap.set(
-          "n",
-          "<leader>hp",
-          require("gitsigns").preview_hunk,
-          { buffer = bufnr, desc = "Preview git hunk" }
-        )
-
-        -- don't override the built-in and fugitive keymaps
-        local gs = package.loaded.gitsigns
-        vim.keymap.set({ "n", "v" }, "]c", function()
-          if vim.wo.diff then
-            return "]c"
-          end
-          vim.schedule(function()
-            gs.next_hunk()
-          end)
-          return "<Ignore>"
-        end, { expr = true, buffer = bufnr, desc = "Jump to next hunk" })
-        vim.keymap.set({ "n", "v" }, "[c", function()
-          if vim.wo.diff then
-            return "[c"
-          end
-          vim.schedule(function()
-            gs.prev_hunk()
-          end)
-          return "<Ignore>"
-        end, { expr = true, buffer = bufnr, desc = "Jump to previous hunk" })
-      end,
-    },
-  },
 
   {
     "EdenEast/nightfox.nvim",
@@ -315,18 +276,6 @@ require("lazy").setup({
       vim.cmd.colorscheme("nightfox")
     end,
   },
-
-  {
-    -- Add indentation guides even on blank lines
-    "lukas-reineke/indent-blankline.nvim",
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
-    main = "ibl",
-    opts = {},
-  },
-
-  -- "gc" to comment visual regions/lines
-  { "numToStr/Comment.nvim", opts = {} },
 
   -- formatting
   {
@@ -500,7 +449,7 @@ vim.o.hlsearch = true
 -- Make line numbers default
 vim.wo.number = true
 
--- Enable mouse mode
+-- Disable mouse mode
 vim.o.mouse = ""
 
 -- Sync clipboard between OS and Neovim.
