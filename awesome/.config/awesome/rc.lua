1ce65a897b706574972f24f80b2dff87795e76bf
Eric Lavigne <hi_eric@hotmail.com>
2024-08-20 21:49:31 -0400

    Remove unused vicious library

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -15,9 +15,6 @@ local naughty = require("naughty")
 local menubar = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup")
 
-local vicious = require("vicious")
-vicious.contrib = require("vicious.contrib")
-
 -- Enable hotkeys help widget for VIM and other apps
 -- when client with a matching name is opened:
 require("awful.hotkeys_popup.keys")
