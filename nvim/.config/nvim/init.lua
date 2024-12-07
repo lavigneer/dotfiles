16c9a76f20d8f357b2d681dc2ebb1799fbddba90
Eric Lavigne <hi_eric@hotmail.com>
2024-12-07 08:57:08 -0500

    Disable treesitter on large files to avoid slowdowns

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -749,7 +749,16 @@ require("lazy").setup({
       require("nvim-treesitter.configs").setup({
         ensure_installed = { "bash", "c", "html", "lua", "markdown", "markdown_inline", "vim", "vimdoc" },
         auto_install = true,
-        highlight = { enable = true },
+        highlight = {
+          enable = true,
+          disable = function(_, buf)
+            local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
+          end,
+        },
         indent = { enable = true },
         textobjects = {
           select = {
