6c3f40308fdc67378f7107de7f8e5c7333e913a8
Eric Lavigne <hi_eric@hotmail.com>
2025-08-10 14:28:26 -0600

    Tweak picker settings

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,3 +1,9 @@
 return {
   { "folke/flash.nvim", enabled = false },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = { enabled = false },
+    },
+  },
 }
--- nvim/.config/nvim/lua/plugins/picker.lua
+++ nvim/.config/nvim/lua/plugins/picker.lua
@@ -2,8 +2,9 @@ return {
   {
     "folke/snacks.nvim",
     keys = {
-      { "<leader><space>", LazyVim.pick("files"), desc = "Find Files (Root Dir)" },
-      { "<leader>sf", LazyVim.pick("files"), desc = "Find Files (Root Dir)" },
+      { "<leader><space>", LazyVim.pick("files", { cmd = "rg", hidden = true }), desc = "Find Files (Root Dir)" },
+      { "<leader>sf", LazyVim.pick("files", { cmd = "rg", hidden = true }), desc = "Find Files (Root Dir)" },
+      { "<leader>sg", LazyVim.pick("live_grep", { cmd = "rg", hidden = true }), desc = "Grep (Root Dir)" },
     },
   },
 }
