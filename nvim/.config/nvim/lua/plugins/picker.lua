c4912c10079fd311f9d184b016499f781036c8f0
Eric Lavigne <hi_eric@hotmail.com>
2025-09-04 07:22:19 -0600

    Update plugins and fix some keymaps

--- nvim/.config/nvim/lua/plugins/picker.lua
+++ nvim/.config/nvim/lua/plugins/picker.lua
@@ -2,9 +2,15 @@ return {
   {
     "folke/snacks.nvim",
     keys = {
-      { "<leader><space>", LazyVim.pick("files", { cmd = "rg", hidden = true }), desc = "Find Files (Root Dir)" },
-      { "<leader>sf", LazyVim.pick("files", { cmd = "rg", hidden = true }), desc = "Find Files (Root Dir)" },
-      { "<leader>sg", LazyVim.pick("live_grep", { cmd = "rg", hidden = true }), desc = "Grep (Root Dir)" },
+      {
+        "<leader><space>",
+        function()
+          Snacks.picker.buffers({ hidden = true, nofile = true })
+        end,
+        desc = "Find Files (Root Dir)",
+      },
+      { "<leader>sf", LazyVim.pick("files", { cmd = "rg", hidden = true, root = false }), desc = "Find Files (Root Dir)" },
+      { "<leader>sg", LazyVim.pick("live_grep", { cmd = "rg", hidden = true, root = false }), desc = "Grep (Root Dir)" },
     },
   },
 }
